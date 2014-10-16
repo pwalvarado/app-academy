1919ff53a53a0f01ac4bd6de72298f5b7686672e
David Runger <daverunger@gmail.com>
2014-10-15 17:28:28 -0700

    computer player captures highest-value capturable piece

--- week2/w2d2-3/chess/computer_player.rb
+++ week2/w2d2-3/chess/computer_player.rb
@@ -1,3 +1,11 @@
+VALUE = {
+  :queen => 9.0,
+  :rook => 5.0,
+  :bishop => 3.15,
+  :knight => 3.0,
+  :pawn => 1.0
+}
+
 class ComputerPlayer
   attr_accessor :name, :color, :board
 
@@ -17,8 +25,13 @@ class ComputerPlayer
       dest = move.last
       !board[dest].nil? && board[dest].color != self.color
     end
-    return capturing_moves.sample unless capturing_moves.empty?
-    
+    sorted_capturing_moves = capturing_moves.sort_by do |move|
+      dest = move.last
+      piece_type_to_capture = board[dest].class.to_s.downcase.to_sym
+      VALUE[piece_type_to_capture]
+    end
+    return sorted_capturing_moves.last unless sorted_capturing_moves.empty?
+
     all_moves.sample
   end
 end
