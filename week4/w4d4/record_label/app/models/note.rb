c73330d936566422a0cbcf4afca3344b359db04d
David Runger <daverunger@gmail.com>
2014-10-30 19:42:06 -0700

    add email to note display

--- week4/w4d4/record_label/app/models/note.rb
+++ week4/w4d4/record_label/app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ActiveRecord::Base
   validates :user_id, :track_id, :body, presence: true
   validates :body, uniqueness: { scope: [:user_id, :track_id] }
-
   belongs_to :user
   belongs_to :track
+  delegate :email, to: :user, prefix: true
 end
