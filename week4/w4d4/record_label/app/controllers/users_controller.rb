72ab8cacea1e82eed2a011efb9da124e4a8d077d
David Runger <daverunger@gmail.com>
2014-10-30 21:37:44 -0700

    redirect to root_url after user creation/login

--- week4/w4d4/record_label/app/controllers/users_controller.rb
+++ week4/w4d4/record_label/app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in!(@user)
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:errors] ||= []
       flash.now[:errors] += @user.errors.full_messages
