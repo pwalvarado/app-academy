c54f91c1b9ddaf8c535411a1ecc9e48c272d9ef8
David Runger <daverunger@gmail.com>
2014-11-05 11:17:39 -0800

    extract route param regex matching into own method

--- week5/w5d2/rails_lite/lib/phase6/router.rb
+++ week5/w5d2/rails_lite/lib/phase6/router.rb
@@ -9,16 +9,19 @@ module Phase6
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
-        !!(req.path =~ pattern && req.request_method.downcase.to_sym == http_method)
+      !!(req.path =~ pattern) && req.request_method.downcase.to_sym == http_method
     end
 
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
-        req.path =~ pattern
-        route_params = Hash[$~.names.map{|x| [x.to_s, $~[x]]}]
-        controller = controller_class.new(req, res, route_params)
+        controller = controller_class.new( req, res, route_params(req) )
         controller.invoke_action(action_name)
+    end
+
+    def route_params(req)
+        route_matchdata = req.path.match(pattern)
+        Hash[ route_matchdata.names.zip( route_matchdata.captures ) ]
     end
   end
 
